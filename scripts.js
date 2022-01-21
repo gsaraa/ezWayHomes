7ecd0fd1cf39827675002a1d4f69b52a0bbfd82d
Sara <amiri_gs@hotmail.com>
2022-01-21 13:24:47 -0500

    started image carousel

--- scripts.js
+++ scripts.js
@@ -66,5 +66,142 @@
 //     }
 // })
 
+// ==================== NEW CODE
+
+// Declaring variables
+// const itemClassName = 'gallerImg'
+// const items = document.getElementsByClassName(itemClassName);
+const items = document.querySelectorAll('.galleryImg');
+totalItems = items.length,
+slide = 0,
+moving = true;
+console.log("what up");
+console.log(items);
+
+// Setting class names
+function setInitialClasses() {
+    console.log(totalItems);
+    items[totalItems - 1].classList.add('prev');
+    items[0].classList.add('active');
+    items[1].classList.add('next');
+}
+
+// Caching buttons and adding event listeners to them
+function setEventListeners() {
+    const next = document.querySelector('.button--next')[0];
+    
+    const prev = document.querySelector('.button--prev')[0];
+
+    next.addEventListener('click', moveNext);
+    prev.addEventListener('click', movePrev);
+}
+
+// Creating functions to attach to buttons
+
+// Function to move to the next slide
+function moveNext() {
+    // Checking if moving
+    if (!moving) {
+
+        // If it's the last slide, reset to ), else +1
+        if (slide === (totalItems - 1)) {
+            slide = 0;
+        } else {
+            slide++;
+        }
+
+        // This function moves carousel to updated slide
+        moveCarouselTo(slide);
+    }
+}
+
+// Function to move to the previous slide
+function movePrev() {
+    // Check if moving
+    if (!moving) {
+        // If it's the first slide, set as the last slide, else -1
+        if (slide === 0) {
+            slide = (totalItems - 1);
+        } else {
+            slide--;
+        }
+
+        // This function moves the carousel to updated slide
+        moveCarouselTo(slide);
+    }
+}
+
+// This function get "moving" to true when triggered, then back to false after our transition is finished
+function disableInteraction() {
+    // Set moving to true for the same duration as our transition
+    // (0.5s = 500ms)
+
+    moving = true;
+    
+    // setTimeOut runs its function once after the given time
+
+    setTimeout(function() {
+        moving = false;
+    }, 500);
+}
+
+// This function controls the whole carousel and takes the slide number as an argument
+function moveCarouselTo(slide) {
+    // This translate to if carousel is moving, if false then allow interaction
+    if(!moving) {
+        // Temporarily disable interactivity
+        disableInteraction();
+
+        // Update the OLD adjecent slides with NEW ones
+        const newPrev = slide - 1;
+        const newNext = slide + 1;
+        const oldPrev = slide - 2;
+        const oldNext = slide + 2;
+
+        // Test if carousel has more than three items
+        if ((totalItems - 1) > 3) {
+            // Checks and updates if the new slides are out of bounds 
+            if (newPrev <= 0) {
+                oldPrev = (totalItems - 1);
+            } else if (newNext >= (totalItems - 1)) {
+                oldNext = 0;
+            }
+
+            // Checks and updates if slide is at the beginning or end
+            if (slide === 0) {
+                newPrev = (totalItems - 1);
+                oldPrev = (totalItems - 2);
+                oldNext = (slide + 1); 
+            } else if (slide === (totalItems - 1)) {
+                newPrev = (slide - 1);
+                newNext = 0;
+                oldNext = 1;
+            }
+
+            // Adding or removing classes will trigger the transitions
+
+            // Reseting old next/prev elements to default classes
+            items[oldPrev].className = "galleryImg";
+            items[oldNext].className = 'galleryImg';
+
+            // Add New Classes
+            items[newPrev].className = "galleryImg" + "prev";
+            items[slide].className = "gallerImg" + "active";
+            items[newNext].className = "galleryImg" + "next";
+        }
+    }
+}
+
+// Function to initialize everything
+function init() {
+    setInitialClasses();
+    setEventListeners();
+
+    // Set moving to false so that the carousel becomes inactive
+    moving = false;
+}
+
+// Calling init function 
+init();
 
 AOS.init();
