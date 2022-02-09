cc5029c41bb57198a07c4a9d4770ccbc97607d32
Sara <amiri_gs@hotmail.com>
2022-02-08 19:50:04 -0500

    tried a different approach to the carousel

--- scripts.js
+++ scripts.js
@@ -99,107 +99,129 @@ function setEventListeners() {
 // Creating functions to attach to buttons
 
 // Function to move to the next slide
-function moveNext() {
-    // Checking if moving
-    if (!moving) {
+// function moveNext() {
+//     // Checking if moving
+//     if (!moving) {
 
-        // If it's the last slide, reset to ), else +1
-        if (slide === (totalItems - 1)) {
-            slide = 0;
-        } else {
-            slide++;
-        }
+//         // If it's the last slide, reset to ), else +1
+//         if (slide === (totalItems - 1)) {
+//             slide = 0;
+//         } else {
+//             slide++;
+//         }
 
-        // This function moves carousel to updated slide
-        moveCarouselTo(slide);
-    }
-}
+//         // This function moves carousel to updated slide
+//         moveCarouselTo(slide);
+//     }
+// }
 
-// Function to move to the previous slide
-function movePrev() {
-    // Check if moving
-    if (!moving) {
-        // If it's the first slide, set as the last slide, else -1
-        if (slide === 0) {
-            slide = (totalItems - 1);
-        } else {
-            slide--;
-        }
+// // Function to move to the previous slide
+// function movePrev() {
+//     // Check if moving
+//     if (!moving) {
+//         // If it's the first slide, set as the last slide, else -1
+//         if (slide === 0) {
+//             slide = (totalItems - 1);
+//         } else {
+//             slide--;
+//         }
 
-        // This function moves the carousel to updated slide
-        moveCarouselTo(slide);
-    }
-}
+//         // This function moves the carousel to updated slide
+//         moveCarouselTo(slide);
+//     }
+// }
 
-// This function get "moving" to true when triggered, then back to false after our transition is finished
-function disableInteraction() {
-    // Set moving to true for the same duration as our transition
-    // (0.5s = 500ms)
+// // This function get "moving" to true when triggered, then back to false after our transition is finished
+// function disableInteraction() {
+//     // Set moving to true for the same duration as our transition
+//     // (0.5s = 500ms)
 
-    moving = true;
+//     moving = true;
     
-    // setTimeOut runs its function once after the given time
+//     // setTimeOut runs its function once after the given time
 
-    setTimeout(function() {
-        moving = false;
-    }, 500);
-}
+//     setTimeout(function() {
+//         moving = false;
+//     }, 500);
+// }
 
-// This function controls the whole carousel and takes the slide number as an argument
-function moveCarouselTo(slide) {
-    // This translate to if carousel is moving, if false then allow interaction
-    if(!moving) {
-        // Temporarily disable interactivity
-        disableInteraction();
+// // This function controls the whole carousel and takes the slide number as an argument
+// function moveCarouselTo(slide) {
+//     // This translate to if carousel is moving, if false then allow interaction
+//     if(!moving) {
+//         // Temporarily disable interactivity
+//         disableInteraction();
 
-        // Update the OLD adjecent slides with NEW ones
-        const newPrev = slide - 1;
-        const newNext = slide + 1;
-        const oldPrev = slide - 2;
-        const oldNext = slide + 2;
+//         // Update the OLD adjecent slides with NEW ones
+//         const newPrev = slide - 1;
+//         const newNext = slide + 1;
+//         const oldPrev = slide - 2;
+//         const oldNext = slide + 2;
 
-        // Test if carousel has more than three items
-        if ((totalItems - 1) > 3) {
-            // Checks and updates if the new slides are out of bounds 
-            if (newPrev <= 0) {
-                oldPrev = (totalItems - 1);
-            } else if (newNext >= (totalItems - 1)) {
-                oldNext = 0;
-            }
+//         // Test if carousel has more than three items
+//         if ((totalItems - 1) > 3) {
+//             // Checks and updates if the new slides are out of bounds 
+//             if (newPrev <= 0) {
+//                 oldPrev = (totalItems - 1);
+//             } else if (newNext >= (totalItems - 1)) {
+//                 oldNext = 0;
+//             }
 
-            // Checks and updates if slide is at the beginning or end
-            if (slide === 0) {
-                newPrev = (totalItems - 1);
-                oldPrev = (totalItems - 2);
-                oldNext = (slide + 1); 
-            } else if (slide === (totalItems - 1)) {
-                newPrev = (slide - 1);
-                newNext = 0;
-                oldNext = 1;
-            }
+//             // Checks and updates if slide is at the beginning or end
+//             if (slide === 0) {
+//                 newPrev = (totalItems - 1);
+//                 oldPrev = (totalItems - 2);
+//                 oldNext = (slide + 1); 
+//             } else if (slide === (totalItems - 1)) {
+//                 newPrev = (slide - 1);
+//                 newNext = 0;
+//                 oldNext = 1;
+//             }
 
-            // Adding or removing classes will trigger the transitions
+//             // Adding or removing classes will trigger the transitions
 
-            // Reseting old next/prev elements to default classes
-            items[oldPrev].className = "galleryImg";
-            items[oldNext].className = 'galleryImg';
+//             // Reseting old next/prev elements to default classes
+//             items[oldPrev].className = "galleryImg";
+//             items[oldNext].className = 'galleryImg';
 
-            // Add New Classes
-            items[newPrev].className = "galleryImg" + "prev";
-            items[slide].className = "gallerImg" + "active";
-            items[newNext].className = "galleryImg" + "next";
-        }
-    }
-}
+//             // Add New Classes
+//             items[newPrev].className = "galleryImg" + "prev";
+//             items[slide].className = "gallerImg" + "active";
+//             items[newNext].className = "galleryImg" + "next";
+//         }
+//     }
+// }
 
-// Function to initialize everything
-function init() {
-    setInitialClasses();
-    setEventListeners();
+// // Function to initialize everything
+// function init() {
+//     setInitialClasses();
+//     setEventListeners();
 
-    // Set moving to false so that the carousel becomes inactive
-    moving = false;
-}
+//     // Set moving to false so that the carousel becomes inactive
+//     moving = false;
+// }
+
+const buttons = document.querySelectorAll('[data-carouselButton]')
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        const offset = button.dataset.carouselButton === 'next' ? 1 : -1
+
+        const slides = button
+        .closest('[data-carousel]')
+        .querySelector('[data-slides]')
+
+        const activeSlide = slides.querySelector('[data-active]')
+
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+    })
+})
 
 // Calling init function 
 init();
